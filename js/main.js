d830a71c9b5dee54fea45a327b1f8a0f3ffa3041
David Rajchenbach-Teller <dteller@mozilla.com>
2013-01-23 12:01:18 +0100

    [Debug] Better integration of debugging code

--- js/main.js
+++ js/main.js
@@ -883,8 +883,46 @@
   eltFork.addEventListener("click", onfork);
   eltFork.addEventListener("touchend", onfork);
 */
+
+  // Exported code
   window.Piranhas = {
     options: Options,
     statistics: Statistics
   };
+
+  // Debugging code
+  if (window.location.search.length > 1) {
+    console.log("Debugging");
+    (function() {
+      var args = window.location.search.substr(1).split("&");
+      for (var arg of args) {
+        console.log(arg);
+        switch (arg) {
+        case "debug":
+          window.Piranhas.options.debug = true;
+          break;
+        case "nocoll":
+          window.Piranhas.options.debugNoCollisions = true;
+          break;
+        case "nomov":
+          window.Piranhas.options.debugNoMovements = true;
+          break;
+        case "profcoll":
+          window.Piranhas.options.profileCollisions = true;
+          break;
+        case "profmov":
+          window.Piranhas.options.profileMovement = true;
+          break;
+        case "profclean":
+          window.Piranhas.options.profileCleanup = true;
+          break;
+        case "profscore":
+          window.Piranhas.options.profileScore = true;
+          break;
+        }
+      }
+    })();
+  } else {
+    console.log("No debugging");
+  }
 })();
